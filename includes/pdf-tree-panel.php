4660b310db3e342ffcf4c5af9d0f5f8cb917ed78
Patrik <patrikbecher@seznam.cz>
2025-11-08 17:50:10 +0100

    tlačítko na smazání upraveno

--- includes/pdf-tree-panel.php
+++ includes/pdf-tree-panel.php
@@ -8,8 +8,8 @@ extract($panelData);
 ?>
 
 <div id="fileTree">
-    <button id="uploadBtn" class="pdfButton" title="Nahrát PDF">✚</button>
-    <button id="deleteBtn" class="pdfButton" title="Smazat PDF">🗑️</button>
+    <button id="uploadBtn" class="pdfButton" title="Nahrát PDF">+</button>
+    <button id="deleteBtn" class="pdfButton" title="Smazat PDF">−</button>
 
     <!-- Panel pro řazení souborů -->
     <div id="fileSort" class="file-sort">
